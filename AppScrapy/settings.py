8f0109ff63ff99a77a6597f28f10927917f96394
fiter <vadym_prokopec@mail.ru>
2017-02-04 20:16:32 +0200

    Sample json added

--- AppScrapy/settings.py
+++ AppScrapy/settings.py
@@ -14,7 +14,7 @@ BOT_NAME = 'AppScrapy'
 SPIDER_MODULES = ['AppScrapy.spiders']
 NEWSPIDER_MODULE = 'AppScrapy.spiders'
 
-#FEED_EXPORT_ENCODING = 'utf-8'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'AppScrapy (+http://www.yourdomain.com)'
